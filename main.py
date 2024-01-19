d5fa282f163e60185d867aedfe8cb7e2a5518f10
drehnstrom <drehnstrom@gmail.com>
2024-01-19 20:49:58 +0000

    Fixed sessions

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import os
 import yaml
 import vertexai
-from vertexai.preview.language_models import ChatModel, InputOutputTextPair
+from vertexai.preview.language_models import ChatModel, InputOutputTextPair, ChatMessage
 
 app = Flask(__name__)
+app.secret_key = "session_key1"
 
 def get_config_value(config, section, key, default=None):
     """
@@ -27,38 +28,46 @@ MAX_OUTPUT_TOKENS = get_config_value(config, 'palm', 'max_output_tokens', 256)
 TOP_P = get_config_value(config, 'palm', 'top_p', 0.8)
 TOP_K = get_config_value(config, 'palm', 'top_k', 40)
 
+EXAMPLES=get_config_value(config, 'palm', 'examples', [])
+examples = [InputOutputTextPair(input_text=k, output_text=v) for k,v in EXAMPLES.items()]
+     
+@app.route("/", methods = ['POST', 'GET'])
+def main():
+    if request.method == 'POST':
+        input = request.form['input']
+        if input > " ":
+            response = get_response(input)
+        else:
+            response = "Ask me something"
+        input = ""
+    else: 
+        input = ""
+        session.pop('chat_history', None)        
+        response = get_response("Who are you and what can you do?")
 
-vertexai.init(location="us-central1")
-chat_model = ChatModel.from_pretrained("chat-bison@001")
-parameters = {
+
+    model = {"title": TITLE, "subtitle": SUBTITLE, "botname": BOTNAME, "message": response, "input": input, "history": session["chat_history"]}
+    return render_template('index.html', model=model)
+
+
+def get_response(input):
+    vertexai.init(location="us-central1")
+    chat_model = ChatModel.from_pretrained("chat-bison@001")
+    parameters = {
         "temperature": TEMPERATURE,
         "max_output_tokens": MAX_OUTPUT_TOKENS,
         "top_p": TOP_P,
         "top_k": TOP_K
      }
 
-EXAMPLES=get_config_value(config, 'palm', 'examples', [])
-examples = [InputOutputTextPair(input_text=k, output_text=v) for k,v in EXAMPLES.items()]
-     
-chat = chat_model.start_chat(context=CONTEXT, examples=examples)
+    if 'chat_history' in session:
+        chat_history = [ChatMessage(content=items["content"], author=items["author"]) for items in session["chat_history"]]
+        parameters["message_history"] = chat_history
 
-@app.route("/", methods = ['POST', 'GET'])
-def main():
-    if request.method == 'POST':
-        input = request.form['input']
-        response, history = get_response(input)
-    else: 
-        input = ""
-        response, history = get_response("Who are you and what can you do?")
-
-    model = {"title": TITLE, "subtitle": SUBTITLE, "botname": BOTNAME, "message": response, "input": input, "history": history}
-    return render_template('index.html', model=model)
-
-
-def get_response(input):
-    print(chat.send_message(input))
+    chat = chat_model.start_chat(context=CONTEXT, examples=examples,  **parameters)
     response = chat.send_message(input)
-    return response, chat.message_history
+    session["chat_history"] = chat.message_history
+    return response.text
     
 
 if __name__ == '__main__':
